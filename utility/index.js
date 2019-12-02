94dfc0d5160b7195128d762f9a9fe0573dcd44d3
Jeremy Hull <sourdrums@gmail.com>
2019-12-01 22:38:36 -0700

    Add markdownToHtml utility

---
+++ utility/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const createSlug     = require('./createSlug');
+const isNil          = require('./isNil');
+const markdownToHtml = require('./markdownToHtml');
+
+module.exports = {
+    createSlug,
+    isNil,
+    markdownToHtml
+};
---
+++ utility/markdownToHtml.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const marked = require('marked');
+
+const lineBreakRegExp = /\r?\n/g;
+
+function removeLineBreaks(value) {
+    return value.replace(lineBreakRegExp, '');
+}
+
+function markdownToHtml(markdown) {
+    const html = marked(markdown);
+
+    return removeLineBreaks(html);
+}
+
+module.exports = markdownToHtml;
