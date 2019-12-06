186fc340af03658b7755fcadc033f39b2c15c306
Jeremy Hull <sourdrums@gmail.com>
2019-12-06 00:30:31 -0700

    Add utility for setting cache headers

--- utility/index.js
+++ utility/index.js
@@ -1,15 +1,25 @@
 'use strict';
 
-const asMilliseconds = require('./asMilliseconds');
-const cache          = require('./cache');
-const createSlug     = require('./createSlug');
-const isNil          = require('./isNil');
-const markdownToHtml = require('./markdownToHtml');
+const asMilliseconds  = require('./asMilliseconds');
+const asSeconds       = require('./asSeconds');
+const cache           = require('./cache');
+const createETag      = require('./createETag');
+const createSlug      = require('./createSlug');
+const hasForceQuery   = require('./hasForceQuery');
+const isNil           = require('./isNil');
+const isString        = require('./isString');
+const markdownToHtml  = require('./markdownToHtml');
+const setCacheHeaders = require('./setCacheHeaders');
 
 module.exports = {
     asMilliseconds,
+    asSeconds,
     cache,
+    createETag,
     createSlug,
+    hasForceQuery,
     isNil,
-    markdownToHtml
+    isString,
+    markdownToHtml,
+    setCacheHeaders
 };
