43fde27b898575863864342f3d853dbb115b3977
Jeremy Hull <sourdrums@gmail.com>
2019-12-28 21:57:44 -0700

    Add redis host name to environment

--- utility/cache.js
+++ utility/cache.js
@@ -7,6 +7,7 @@ const asSeconds = require('./asSeconds');
 
 const redisCache = cacheManager.caching({
     store: redisStore,
+    host: process.env.REDIS_HOST || 'localhost',
     password: `${process.env.REDIS_PASSWORD}`,
     db: parseInt(`${process.env.REDIS_DB_INDEX || '0'}`, 10),
     ttl: asSeconds(3, 'days')
